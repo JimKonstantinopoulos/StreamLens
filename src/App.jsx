49c3c0d344361aa1150291cf3c3b2de071597118
dimKonstantinopoulos <dimitris.konstantinopoulos@expressdigibooks.com>
2024-10-14 16:00:32 +0300

    Add responsive background to homepage

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.module.css";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return <Homepage />;
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import styles from "./Homepage.module.css";
+
+export default function Homepage() {
+  return (
+    <main>
+      <picture>
+        <source media="(max-width: 320px)" srcSet="../assets/bg-320.webp" />
+        <source media="(max-width: 640px)" srcSet="../assets/bg-640.webp" />
+        <source media="(max-width: 1280px)" srcSet="../assets/bg-1280.webp" />
+        <source media="(max-width: 1920px)" srcSet="../assets/bg-1920.webp" />
+        <img
+          srcSet="../assets/bg.webp"
+          alt="Responsive Background"
+          className={styles.backgroundImage}
+        />
+      </picture>
+    </main>
+  );
+}
