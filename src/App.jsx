4d4fda3700ab36c4b3b0ae26a04002c17b5b3dd4
dimKonstantinopoulos <dimitris.konstantinopoulos@expressdigibooks.com>
2024-11-11 13:50:57 +0200

    Replace ReactRouter with ReactRouter v6.4 approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import "./App.module.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Greet from "./components/Greet";
 import Login from "./components/Login";
 import SignUp from "./components/Signup";
 import ContentPage from "./pages/ContentPage";
 import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+  {
+    path: "/app",
+    element: <ContentPage />,
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
 
 function App() {
   return (
